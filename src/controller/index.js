128817a8c369fccd0e797d36320eb02734bfa30a
nitish <nitish.saini@aajtak.com>
2024-02-22 17:45:31 +0530

    fixing translation

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,4 +1,4 @@
-const translate = require('@vitalets/google-translate-api');
+const {translate} = require('@vitalets/google-translate-api');
 
 const translateController = async (req, res) => {
     const { text } = req.body;
